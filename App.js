49d2936eead2a8ce17d4dbb68ece38200688ce6e
amiguel4 <amiguelm@villanova.edu>
2025-12-09 16:08:21 -0500

    Add App.js

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import EventsScreen from '../NovaCentral/EventsScreen';
+import AthleticsScreen from '../NovaCentral/AthleticsScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AthleticsScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
